ab066b033c902353badaff1b75782147ffe9c1f1
NovoseletskiyVadim <novoseletskiy.v@gmail.com>
2018-10-21 23:33:55 +0300

    Create an associative array

--- homework2/associativeArray.js
+++ homework2/associativeArray.js
@@ -1,11 +1,25 @@
 
 //Add associative array
-console.log("Начало работы с Ассоциатвным массивом")
+console.log("Начало работы с Ассоциатвным массивом");
 var associativeArrayMobileTel={
     ['phone brand']:'nokia',
     ['model']:3310,
     ['color']:'black',
     ['year of issue']:2010,
     ['start price']:100
+};
+console.log('Создание ассоциативного массива и вывод его  значений ');
+console.log(associativeArrayMobileTel.toString());
+if(associativeArrayMobileTel==null){
+    console.error("Ошибка:Массив не создан!!");
 }
+else{
+    for(var i in associativeArrayMobileTel){
+        console.log("Ключ= "+i+"; Значение= "+ associativeArrayMobileTel[i]);
+    }
+}
+
+
+
+
 
