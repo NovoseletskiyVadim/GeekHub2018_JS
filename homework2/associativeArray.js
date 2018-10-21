2f51b0c855c977bc1ade5c2890fa783418ce2beb
NovoseletskiyVadim <novoseletskiy.v@gmail.com>
2018-10-21 23:19:27 +0300

    Add Associative array

---
+++ homework2/associativeArray.js
@@ -0,0 +1,11 @@
+
+//Add associative array
+console.log("Начало работы с Ассоциатвным массивом")
+var associativeArrayMobileTel={
+    ['phone brand']:'nokia',
+    ['model']:3310,
+    ['color']:'black',
+    ['year of issue']:2010,
+    ['start price']:100
+}
+
