7b5988f5070df6e9b58eaf646ae93ce5d48c6528
NovoseletskiyVadim <novoseletskiy.v@gmail.com>
2018-10-22 11:28:49 +0300

    Add method "push"

--- homework2/associativeArray.js
+++ homework2/associativeArray.js
@@ -8,8 +8,10 @@ var associativeArrayMobileTel={
     ['year of issue']:2010,
     ['start price']:100
 };
-console.log('Создание ассоциативного массива и вывод его  значений ');
-console.log(associativeArrayMobileTel.toString());
+console.log('Создание ассоциативного массива и вывод его  значений '+
+associativeArrayMobileTel.toString());
+
+// check array
 if(associativeArrayMobileTel==null){
     console.error("Ошибка:Массив не создан!!");
 }
@@ -17,6 +19,41 @@ else{
     for(var i in associativeArrayMobileTel){
         console.log("Ключ= "+i+"; Значение= "+ associativeArrayMobileTel[i]);
     }
+}
+
+// implementation of methods
+
+// Add method "push"
+
+// associativeArrayMobileTel['key'].push('value');
+// var newValue={'myKey':'myValue'};
+// associativeArrayMobileTel.push("37637643","jdhfjdhfjdfh");
+
+console.warn("Добавдение метода аналог. push. Начало работы.");
+
+var associativeArrayMobileTel={
+    ['phone brand']:'nokia',
+    ['model']:3310,
+    ['color']:'black',
+    ['year of issue']:2010,
+    ['start price']:100,
+
+    myPush:function(myKey,myValue){
+        if(myKey==null||myKey==undefined){
+                console.error("Error:отсутствует ключа!!");
+        }
+        else if(myValue==null||myValue==undefined){
+            console.error("Error:отсутствует значеник  ключа!!");            
+        } 
+        else{
+            return this[myKey]=myValue;  
+
+        }
+    }
+};
+associativeArrayMobileTel.myPush('la la la',12);
+for(var i in associativeArrayMobileTel){
+    console.log("Ключ= "+i+"; Значение= "+ associativeArrayMobileTel[i]);
 }
 
 
