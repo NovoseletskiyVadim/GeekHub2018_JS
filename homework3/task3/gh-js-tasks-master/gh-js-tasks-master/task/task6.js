7911e86f04035e7c1eb8eed8e8141e1cd892f57f
NovoseletskiyVadim <novoseletskiy.v@gmail.com>
2018-10-31 00:35:21 +0300

    Finished work on the task 6. Paragraf 3 of homework 3

--- homework3/task3/gh-js-tasks-master/gh-js-tasks-master/task/task6.js
+++ homework3/task3/gh-js-tasks-master/gh-js-tasks-master/task/task6.js
@@ -34,7 +34,49 @@ var prettyYearTests = [
 
 
 function prettyYear(y) {
-    //TODO
+    var myNumerYear=Number(y);
+    var myFinalResult=false;
+    
+    function myNumberYearToString(x){
+        var myValue=x+"";
+        var myFinishResult;
+        var a,b,c,d;
+
+        a=Number(myValue.charAt(0));
+        b=Number(myValue.charAt(1));
+        c=Number(myValue.charAt(2));
+        d=Number(myValue.charAt(3));
+
+        var  myFinishResult_a;
+        var  myFinishResult_b; 
+        var  myFinishResult_c;
+        var  myFinishResult_d;
+       
+        if(a!=b && a!=c && a!=d){
+                myFinishResult_a=a;
+
+            if(b!=c && b!=d){
+                myFinishResult_b=b;
+
+                if(c!=d){
+                    myFinishResult_c=c;
+                    myFinishResult_d=d
+          
+                }else return myFinishResult=false;
+            }else return myFinishResult=false;
+        }else return myFinishResult=false; 
+        
+        myFinishResult=""+myFinishResult_a+myFinishResult_b+myFinishResult_c+myFinishResult_d;
+        myFinalResult=myFinishResult;
+        return myFinalResult;
+    }
+    
+    for(var i=myNumerYear+1;i<9000;i++){
+        
+        var finishValue=myNumberYearToString(i)
+        if(finishValue===false)continue; 
+        else return myFinalResult=(Number(myNumberYearToString(i)));
+    };    
 }
 
 
