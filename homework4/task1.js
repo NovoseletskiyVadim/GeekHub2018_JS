c8b22657d106805645f88f911d588b228408f150
NovoseletskiyVadim <novoseletskiy.v@gmail.com>
2018-11-12 00:46:12 +0200

    Fix Bug

--- homework4/task1.js
+++ homework4/task1.js
@@ -2,27 +2,41 @@
 
 console.log("Start");
 
-var myRocksWater=[2,1,5,0,3,4,7,2,3,1,0];
+var myRocksWater=[0,10,10,0,5,7,2,0];
+// var myRocksWater=[2,1,5,0,3,4,7,2,3,1,0];
+// var myRocksWater=[0,1,0,1,0,1,0,1,0,1];
+// var myRocksWater=[0,1,2,3,4,5,5,4,3,2,1,0];
+
+
+
 
 var MinValue, startIndex, endIndex=0;
 var myResult=0;
 
 for(var i=0; i<myRocksWater.length;i++){
 
-    if(myRocksWater[i]<=myRocksWater[i+1]) continue;
+    if(myRocksWater[i]<=myRocksWater[i+1]){
+        
+        continue;  
+    } 
+
 
     else if(myRocksWater[i]>myRocksWater[i+1]){
 
         var myArray=[];
         startIndex=i;
-
+       
         for(;i<myRocksWater.length;i++){
+           
             if(myRocksWater[i+1]>= myRocksWater[startIndex]){
                 endIndex=i+1;
                 break;
             }
+            
         }
-        if(endIndex==startIndex){
+         
+        if(endIndex<=startIndex){
+
             i=startIndex+1;
             MinValue=myRocksWater[i];
             
@@ -30,6 +44,7 @@ for(var i=0; i<myRocksWater.length;i++){
                 if(myRocksWater[i]>MinValue&&myRocksWater[i]>myRocksWater[i+1]){
                     endIndex=i; 
                     break;
+
                 }
             }
             myArray=myRocksWater.slice(startIndex,endIndex);
@@ -37,7 +52,8 @@ for(var i=0; i<myRocksWater.length;i++){
             var a=0;
             for(;a<myArray.length;a++){
                 if(myArray[a]>myRocksWater[i])break;                    
-                myResult+=myRocksWater[i]-myArray[a];                
+                myResult+=myRocksWater[i]-myArray[a]; 
+
             }
             
         }else{
