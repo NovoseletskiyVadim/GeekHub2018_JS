013b219031267274236ef05338c6e55cc05e7fa8
NovoseletskiyVadim <novoseletskiy.v@gmail.com>
2019-01-09 16:16:18 +0200

    Add interesting examples (Gulp)

--- InterestingExamples/console.js
+++ InterestingExamples/console.js
@@ -32,3 +32,82 @@ var time = {
   
   console.log(time);
   time.microsecond++;
+
+
+
+// console.log();
+
+// %s -строковое состояние 
+// %o -обьект 
+// %d -decimal / integer
+
+console.log('I like %s but I do not like %s.', 'Skittles', 'pus');
+
+// %c - стили
+console.log('I am a %cbutton', 'color: white; background-color: orange; padding: 2px 5px; border-radius: 2px');
+
+// console.dir();
+
+// console.warn();
+
+// console.table();
+// > console.table(data, ["id", "price"]);
+// !!!! но не более 1000 строк 
+
+// console.assert();
+// условие должно быть ложным 
+console.assert(tx.buyer !== 'WAL0412', tx);
+
+// console.count()
+
+/*
+        работает как счетчик:
+
+    odds: 1
+    odds: 2
+    prime: 1
+    odds: 3
+    multiplesOfFive: 1
+    prime: 2
+    odds: 4
+    prime: 3
+    odds: 5
+    multiplesOfFive: 2
+
+    console.countReset ()-сброс счетчика 
+
+*/ 
+
+for(let i = 0; i < 10000; i++) {
+    if(i % 2) {
+      console.count('odds');
+    }
+    if(!(i % 5)) {
+      console.count('multiplesOfFive');
+    }
+    if(isPrime(i)) {
+      console.count('prime');
+    }
+  }
+
+
+
+// console.trace()
+/*
+    что бы выяснить внутри класса или библиотеки,
+    какой вызов приводит к багу
+
+*/ 
+export default class CupcakeService {
+    
+    constructor(dataLib) {
+      this.dataLib = dataLib;
+      if(typeof dataLib !== 'object') {
+        console.log(dataLib);
+        console.trace();
+      }
+    }
+  
+    // ...
+  
+  }
