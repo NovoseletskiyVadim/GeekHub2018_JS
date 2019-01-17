c2dadb368cbac07989d504ae406fb9b1e2837bed
NovoseletskiyVadim <novoseletskiy.v@gmail.com>
2019-01-17 14:56:12 +0200

    Home Work 13. Finished work.

--- homework13/webpack.config.js
+++ homework13/webpack.config.js
@@ -1,9 +1,71 @@
-var path = require('path');
+const path = require( 'path' );
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  entry: './app/js/main.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+
+module.exports = ( env, options ) => {
+	return {
+		entry: ['./src/js/main.js','./src/sass/style.scss'],
+		
+		output: {
+			path: path.resolve( __dirname, 'dist' ),
+			filename: 'bundle.js',
+		},
+		// devtool:  'cheap-eval-source-map',
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					use: {
+						loader: 'babel-loader',
+					},
+					exclude: /(node_modules)/
+				},
+				{
+					test: /\.scss$/,
+					use: [
+						MiniCssExtractPlugin.loader,
+						{
+							loader: 'css-loader'
+						},
+						{
+							loader: 'postcss-loader',
+							options: {
+								plugins: () => [
+									require('autoprefixer')
+								],
+								sourceMap: true
+							}
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: true
+						
+							}
+						}
+					]
+				},
+				// {
+				// 	test: /\.(png|jpg|gif)$/,
+				// 	use: [
+				// 		{
+				// 			loader: 'file-loader',
+				// 			options: {
+				// 				name: '[name].[ext]',
+				// 				outputPath: 'dist/'
+				// 			}
+				// 		}
+				// 	]
+				// },
+			],
+		},
+		
+		plugins: [
+			new MiniCssExtractPlugin({
+				filename: './style.css',
+				chunkFilename: '[id].css',
+			})
+		],
+		
+	}
 };
